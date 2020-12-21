9370796e5233451df32409b39ee7f7fff162ecea
Your Name <thomas.w.yee@gmail.com>
2020-12-21 12:14:07 -0500

    css update

--- util/createBar1.mjs
+++ util/createBar1.mjs
@@ -1,5 +1,7 @@
 export default function createBar1(index, num, max) {
   let percentage = (num / max) * 100;
   $(`#${index}`).append(`<td class="sent bar"><p>${num}</p></td>`);
-  $("td").last().css("height", `${percentage}%`);
+  $("td")
+    .last()
+    .css("height", `${percentage + 1}%`);
 }
