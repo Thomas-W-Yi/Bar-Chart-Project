3de113dcbff6a00c7a2bea8991956c5d5ed44e98
Your Name <thomas.w.yee@gmail.com>
2021-01-02 11:39:03 -0500

    test

--- util/data.mjs
+++ util/data.mjs
@@ -1,91 +1,10 @@
-let data = $("#multipArray")
-    .val()
-    .split("],")
-    .map((x) => {
-      return x.split(",");
-    })
-    .map((x) => {
-      return x.map((y) => {
-        return y
-          .split("")
-          .filter((z) => {
-            return z === "[" || z === "]" ? false : true;
-          })
-          .join("");
-      });
-    }),
-  options = {
-    graphHeight: "300px",
-    graphWidth: "600px",
-    barColor: ["#e27d60", "#e8a87c", "#41b3a3", "#8d8741", "#659dbd"],
-    barTextColor: "black",
-    barTextPosition: { top: "0", middle: "50%", bottom: "100%" },
-    valueFontSize: "1rem",
-    titleTextColor: "black",
-    titlePosition: "relative",
-    titleFontSize: "2rem",
-    spacing: data[0].length > 1 ? 600 / data[0].length : 600 / data.length,
-    titleTextTransform: "uppercase",
-    xTextColor: "black",
-    xFontSize: "1rem",
-    valuePosition: "center",
-  },
-  element = "tbody";
-
-$("#singleArray").on("keypress", (e) => {
-  e.which === 13 ? (data = $("#dataInput").val()) : null;
-  console.log(data);
-});
-
-$("#multipArray").on("keypress", (e) => {
-  e.which === 13
-    ? (data = $("#multipArray")
-        .val()
-        .split(";")
-        .map((x) => {
-          return x.split(",");
-        }))
-    : null;
-});
-
-$("#graphHeight").on("keypress", (e) => {
-  e.which === 13 ? (options.graphHeight = $("#graphHeight").val()) : null;
-});
-$("#graphWidth").on("keypress", (e) => {
-  e.which === 13 ? (options.graphWidth = $("#graphWidth").val()) : null;
-});
-$("#labelColor").on("keypress", (e) => {
-  e.which === 13 ? (options.barTextColor = $("#labelColor").val()) : null;
-});
-$("#Spacing").on("keypress", (e) => {
-  e.which === 13 ? (options.Spacing = $("#Spacing").val()) : null;
-});
-$("#fontSize").on("keypress", (e) => {
-  e.which === 13 ? (options.titleFontSize = $("#titleFontSize").val()) : null;
-});
-$("#fontColor").on("keypress", (e) => {
-  e.which === 13 ? (options.titleTextColor = $("#titleTextColor").val()) : null;
-});
-$("#element").on("keypress", (e) => {
-  e.which === 13 ? (element = $("#element").val()) : null;
-});
-
-export { data, options, element };
-
-let value = "[[1,6,10], [2,7,15], [3,8,20],[4,9,25],[5,10,30]]";
-let arr = value
-  .split("],")
-  .map((x) => {
-    return x.split(",");
-  })
-  .map((x) => {
-    return x.map((y) => {
-      return y
-        .split("")
-        .filter((z) => {
-          return z === "[" || z === "]" ? false : true;
-        })
-        .join("");
-    });
-  });
-console.log(arr);
+const createPoint = (x, y) => Object.freeze([x, y]);
+const movePointBy = ([x, y], dx, dy) => {
+  return Object.freeze([x + dx, y + dy]);
+};
+let point = createPoint(0, 0);
+console.log(point);
+point = movePointBy(point, 5, 5);
+console.log(point);
+point = movepointBy(point, -2, 2);
+console.log(point);
